a86c2e6f5d0417bd174552300fa20ab96c1b3ba1
Parker Wild <parkerwild96@gmail.com>
2018-06-27 13:40:33 -0400

    Google Map functionality
    Imported Google Maps API Wrapper, added map functionality.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+/*******************************************************************************
+/* Developer: Parker Wild
+/* GitHub:    @pswild
+/* Description: A React component with interactive congressional districts built
+/* on top of Google Maps Javascript API using FullstackReact's
+/* "google-maps-react" package. The Google Fusion Table layer provided by
+/* Sharon Machlis, using Cartographic Boundary KML Files to denote district
+/* boundaries taken from the United States Census Bureau on the 115th Congress.
+*******************************************************************************/
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// Import Google Maps API Wrapper
+import { Map, GoogleApiWrapper } from 'google-maps-react'
 
-const apiKey = 'AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo';
+// Google Maps API Key from FullstackReact
+const googleMapsAPIKey = 'AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo';
 
-class App extends Component {
+// MapContainer component
+export class MapContainer extends Component {
   render() {
     return (
       <div className="App">
@@ -13,12 +27,15 @@ class App extends Component {
           <h1 className="App-title">Welcome to the Princeton Election Consortium</h1>
           <p className="App-subtitle">Featuring an interactive React component to identifies competitive congressional districts for the 2018 midterm elections.</p>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Map className="App-map" google={this.props.google} zoom={14}>
+          
+        </Map>
       </div>
     );
   }
 }
 
-export default App;
+// Export High-Order Component (HOC) with GoogleAPIWrapper using API key
+export default GoogleApiWrapper({
+  apiKey : googleMapsAPIKey
+}) (MapContainer)
