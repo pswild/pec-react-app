2c430761d630c14c637210be1b873a355496ae49
Parker Wild <parkerwild96@gmail.com>
2018-06-28 15:12:48 -0400

    Functioning Fusion Tables Layer
    Congressional Districts outlined, those which are competitive are highlighted in red. For browsers with "Navigator" functionality, the app finds the user's location, drops a marker, pans and zooms in to help identify nearby districts.

--- src/App.js
+++ src/App.js
@@ -10,72 +10,155 @@
 /* package.
 *******************************************************************************/
 
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-// Import Google Maps API Wrapper
-import { Map, GoogleApiWrapper } from 'google-maps-react';
-// Import Google Fusion Tables Layer Wrapper
-// import { GoogleMap, FusionTablesLayer } from "react-google-maps";
+
 // Import React DOM
-// import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
+// Import Google Maps API Wrapper
+import { GoogleApiWrapper } from 'google-maps-react';
 
 // Google Maps API Key from FullstackReact
 const gAPIKey = 'AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo';
 // Fusion Table ID from Sharon Machlis
 const ftID = '1YR6Oe72NnBYAG3_Ezzg3jaq3R69u4rdvyUZ7fxUZ';
 
+// Map component
+export class Map extends React.Component {
+
+  // Creates a stateful map
+  constructor(props) {
+    super(props);
+    this.state = {
+      zoom : 4.9,
+      currentLocation : {
+        lat : 39.8283,
+        lng : -98.5795
+      }
+    }
+  }
+
+  // Place a marker
+  mark() {
+    // Sets props to Google and Google Maps
+    const google = this.props.google;
+    const maps = google.maps;
+
+    // Place marker at coordinates
+    const coords = this.state.currentLocation;
+    var marker = new google.maps.Marker({
+      position: coords,
+      map: this.map
+    });
+
+    // Pan to coordinates
+    this.map.panTo(coords);
+    this.map.setZoom(10);
+  }
+
+  // Loads Google API map when React component updated
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.google !== this.props.google) {
+      this.loadMap();
+    }
+    // Detect changes in location
+    if (prevState.currentLocation !== this.state.currentLocation) {
+      this.mark();
+    }
+  }
+  // Loads Google API map if React component already mounted
+  componentDidMount() {
+    // Attempt HTML5 Geolocation
+    if (navigator && navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        // Obtain current coordinates
+        const coords = pos.coords;
+        this.setState({
+          currentLocation: {
+            lat : coords.latitude,
+            lng : coords.longitude
+          }
+        });
+      }, (err) => {
+        // Browser does not support location services
+      });
+    }
+    // Load map
+    this.loadMap();
+  }
+
+  // Load map into rendered React component
+  loadMap() {
+    // Confirm Google API is available
+    if (this.props && this.props.google) {
+      // Sets props to Google and Google Maps
+      const google = this.props.google;
+      const maps = google.maps;
+      // Reference to DOM component where map is placed
+      const mapRef = this.refs.map;
+      const node = ReactDOM.findDOMNode(mapRef);
+      // Use variables from this.props to maintain state
+      const zoom = this.state.zoom;
+      const { lat, lng } = this.state.currentLocation;
+      const center = new maps.LatLng(lat, lng);
+
+      // Map configuration object
+      const mapConfig = Object.assign({}, {
+        zoom : zoom,
+        center : center
+      });
+
+      // Create new map
+      this.map = new maps.Map(node, mapConfig);
+
+      // Add Fusion Tables Layer
+      var layer = new google.maps.FusionTablesLayer({
+        query : {
+          select : 'geometry',
+          from : ftID
+        },
+        styles : [{
+          where : 'CookRatingCode < 1',
+          polygonOptions : {
+            fillOpacity : '0',
+          }
+        }]
+      });
+      layer.setMap(this.map);
+    }
+  }
+
+  render() {
+    const style = {
+      width : '100vw',
+      height : '100vh'
+    }
+
+    return (
+      <div ref="map" style={style}>Loading...</div>
+    )
+  }
+}
+
 // MapContainer component
-export class MapContainer extends Component {
-
-  // // Updates Google API map when component updated
-  // componentDidUpdate(prevProps, prevState) {
-  //   if (prevProps.google !== this.props.google) {
-  //     this.loadMap();
-  //   }
-  // }
-  // componentDidMount() {
-  //   this.loadMap();
-  // }
-  //
-  // // Called after component has been rendered
-  // loadMap() {
-  //   // Confirm Google API is available
-  //   if (this.props && this.props.google) {
-  //     // Sets props to Google Map
-  //     const {google} = this.props;
-  //     const maps = google.maps;
-  //
-  //     // Reference to DOM component where map is placed
-  //     const mapRef = this.refs.map;
-  //     const node = ReactDOM.findDOMNode(mapRef);
-  //
-  //
-  //   }
-  // }
-
-  
+export class MapContainer extends React.Component {
 
   render() {
     if (!this.props.loaded) {
       return <div>Loading...</div>
     }
-    return (
+
+    return(
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to the Princeton Election Consortium</h1>
           <p className="App-subtitle">Featuring an interactive React component to identifies competitive congressional districts for the 2018 midterm elections.</p>
         </header>
-        <Map
-          className="App-map"
-          google={this.props.google}
-          initialCenter={{
-            lat : 39.8283,
-            lng : -98.5795
-          }}
-          zoom={5}>
-        </Map>
+        <div>
+          <Map google={this.props.google}></Map>
+        </div>
       </div>
     );
   }
@@ -83,5 +166,5 @@ export class MapContainer extends Component {
 
 // Export High-Order Component (HOC) with GoogleAPIWrapper using API key
 export default GoogleApiWrapper({
-  apiKey : gAPIKey,
+  apiKey : gAPIKey
 }) (MapContainer)
